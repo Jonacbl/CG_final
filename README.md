675c082f38342fd6822d5386d0af5d7c68cd02e1
Jonacbl <chenblin23@mail2.sysu.edu.cn>
2024-12-10 13:17:20 +0800

    models&ground

---
+++ README.md
@@ -0,0 +1,138 @@
+目前是final Project
+
+**Collaborators**：
+
+- [Jonacbl](https://github.com/Jonacbl)
+- [lagrange](https://github.com/lxl1437)
+- [BaiHYF](https://github.com/BaiHYF)
+- [Charles4731](https://github.com/Charles4731)
+
+
+**加载 Minecraft 方块模型**
+
+首先声明需要使用的方块
+
+```cpp
+int main() {
+    // ......
+    // load models
+    Model grass_cube("resources/grass_cube/scene.gltf");
+    Model stone_cube("resources/stone_cube/scene.gltf");
+    Model wood_cube("resources/wood_cube/scene.gltf");
+    Model brick_cube("resources/brick_cube/scene.gltf");
+    Model stone_brick_cube("resources/stone_brick_cube/scene.gltf");
+    Model sand_cube("resources/sand_cube/scene.gltf");
+
+    // .......
+}
+```
+
+之后在渲染循环中使用 `draw_cube` 函数，在指定位置绘制对应的方块。
+
+参数说明:
+```cpp
+void draw_cube(Shader &ourShader, Camera &camera, glm::vec3 lightDir, glm::vec3 worldPos, Model cube, bool ro=false)
+```
+前三个 `shader`, `camera`, `lightDir` 应该不用改，直接用前面代码定义的即可。
+
+- `worldPos` 为将被绘制方块的 世界坐标(World position)
+- `cube` 就是要绘制的方块模型，可以是 grass_cube, stone_cube, wood_cube, brick_cube, stone_brick_cube, sand_cube 之一，目前我只加了这几个。
+- `ro` 目前是一个 bool 值，仅在 cube 为 grass_cube 或 wood_cube 时需被设置为 true
+
+```cpp
+// main.cpp
+
+while (!glfwWindowShouldClose(window))
+{
+    // ......
+
+    // render
+    // ------
+    glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+    {
+        draw_cube(ourShader, camera, lightDir, glm::vec3(3.0f, -5.0f, 3.0f), grass_cube, true);
+        draw_cube(ourShader, camera, lightDir, glm::vec3(2.0f, -5.0f, 3.0f), brick_cube);
+        draw_cube(ourShader, camera, lightDir, glm::vec3(1.0f, -5.0f, 3.0f), stone_cube);
+        draw_cube(ourShader, camera, lightDir, glm::vec3(3.0f, -5.0f, 2.0f), stone_brick_cube);
+        draw_cube(ourShader, camera, lightDir, glm::vec3(2.0f, -5.0f, 2.0f), wood_cube, true);
+        draw_cube(ourShader, camera, lightDir, glm::vec3(1.0f, -5.0f, 2.0f), sand_cube);
+    }
+
+    ...
+}
+```
+
+Over
+
+----
+
+**已完成部分**
+
+- assimp加载静态模型
+- assimp加载动态模型
+- skybox加载天空盒
+
+
+
+在**解决方案资源管理器右键**-**属性**添加对应的**包含文件**路径和**库文件**路径：
+
+```
+./include
+./lib
+```
+
+![](./README/include.png)
+
+链接器-输入中添加：
+
+```
+glfw3.lib
+assimp-vc143-mtd.lib
+```
+
+![](./README/linker.png)
+
+即可在`.sln`运行中运行。
+目前是final Project
+
+**Collaborators**：
+
+- [Jonacbl](https://github.com/Jonacbl)
+- [lagrange](https://github.com/lxl1437)
+- [BaiHYF](https://github.com/BaiHYF)
+- [Charles4731](https://github.com/Charles4731)
+
+
+
+**已完成部分**
+
+- assimp加载静态模型
+- assimp加载动态模型
+- skybox加载天空盒
+- blinn-phong光照
+- 交互摄像机
+
+
+
+在**解决方案资源管理器右键**-**属性**添加对应的**包含文件**路径和**库文件**路径：
+
+```
+./include
+./lib
+```
+
+![](./README/include.png)
+
+链接器-输入中添加：
+
+```
+glfw3.lib
+assimp-vc143-mtd.lib
+```
+
+![](./README/linker.png)
+
+即可在`.sln`运行中运行。
---
+++ main.cpp
@@ -0,0 +1,476 @@
+#define _CRT_SECURE_NO_WARNINGS // SOS
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <learnopengl/shader_m.h>
+#include <learnopengl/camera.h>
+
+#include <learnopengl/model_animation.h>
+#include <learnopengl/animator.h>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
+unsigned int loadCubemap(vector<std::string> faces);
+
+// settings
+const unsigned int SCR_WIDTH = 1600;
+const unsigned int SCR_HEIGHT = 900;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+const glm::vec3 cameraPos = glm::vec3(0.0f, 1.0f, -10.0f);
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+/// @brief Draw a cube, at the given position, read README for more details
+/// @param ourShader shader
+/// @param camera    camera
+/// @param lightDir  light direction
+/// @param worldPos  world position of to draw the model
+/// @param cube      cube model
+/// @param ro        a control flag, by default false
+void draw_cube(Shader &ourShader, Camera &camera, glm::vec3 lightDir, glm::vec3 worldPos, Model cube, bool ro=false) {
+    ourShader.use();
+
+    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    glm::mat4 view = camera.GetViewMatrix();
+    ourShader.setMat4("projection", projection);
+    ourShader.setMat4("view", view);
+    ourShader.setVec3("viewPos", camera.Position);
+    ourShader.setVec3("lightDirection", lightDir);
+
+    glm::mat4 modelMatrix = glm::mat4(1.0f);
+
+    modelMatrix = glm::translate(modelMatrix, worldPos);
+    modelMatrix = glm::translate(modelMatrix, cameraPos);
+    if (ro) {
+        modelMatrix = glm::rotate(modelMatrix, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    }
+    modelMatrix = glm::scale(modelMatrix, glm::vec3(0.5f, 0.5f, 0.5f));
+    ourShader.setMat4("model", modelMatrix);
+
+    cube.Draw(ourShader);
+}
+
+
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Final", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+    stbi_set_flip_vertically_on_load(true);
+
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+
+    // build and compile shaders
+    // -------------------------
+    Shader ourShader("shader/blinn-phong.vs", "shader/blinn-phong.fs");
+    Shader skyboxShader("shader/skybox.vs", "shader/skybox.fs");
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float skyboxVertices[] = {
+        // positions
+        -1.0f,  1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+
+        -1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+
+        1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+
+        -1.0f, -1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+
+        -1.0f,  1.0f, -1.0f,
+        1.0f,  1.0f, -1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f, -1.0f,
+
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+        1.0f, -1.0f,  1.0f
+    };
+
+    // skybox VAO
+    unsigned int skyboxVAO, skyboxVBO;
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
+    glBindVertexArray(skyboxVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    // load textures
+    // -------------
+    vector<std::string> faces
+    {
+        "resources/textures/skybox/right.jpg",
+        "resources/textures/skybox/left.jpg",
+        "resources/textures/skybox/bottom.jpg", // SOS
+        "resources/textures/skybox/top.jpg", // SOS
+        "resources/textures/skybox/front.jpg",
+        "resources/textures/skybox/back.jpg"
+    };
+    unsigned int cubemapTexture = loadCubemap(faces);
+
+    // shader configuration
+    // --------------------
+
+    skyboxShader.use();
+    skyboxShader.setInt("skybox", 0);
+
+    //parallel light
+    glm::vec3 lightDir(0.0f, -1.0f, -1.0f);
+
+    // load models
+    // -----------
+    Model ourModel("resources/autumn-house/source/House_scene_01.fbx");
+    ourModel.CalculateCenter();
+    
+    //animation model
+    //Model aModel("resources/vampire/dancing_vampire.dae");
+    //Animation danceAnimation("resources/vampire/dancing_vampire.dae", &aModel);
+    //Animator animator(&danceAnimation);
+
+    Model aModel("resources/chicken-rig/source/Chicken.FBX");
+    Animation danceAnimation("resources/chicken-rig/source/Chicken.FBX", &aModel);
+    Animator animator(&danceAnimation);
+    aModel.CalculateCenter();
+
+
+    //minecraft_cube
+    Model grass_cube("resources/grass_cube/scene.gltf");
+    Model stone_cube("resources/stone_cube/scene.gltf");
+    Model wood_cube("resources/wood_cube/scene.gltf");
+    Model brick_cube("resources/brick_cube/scene.gltf");
+    Model stone_brick_cube("resources/stone_brick_cube/scene.gltf");
+    Model sand_cube("resources/sand_cube/scene.gltf");
+    // draw in wireframe
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // per-frame time logic
+        // --------------------
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // input
+        // -----
+        processInput(window);
+		  //animator.UpdateAnimation(deltaTime/1.5f);
+          //beeAnimator.UpdateAnimation(deltaTime);
+ 
+        // render
+        // ------
+        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        //static model of House
+        {
+            ourShader.use();
+
+            // view/projection transformations
+            glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+            glm::mat4 view = camera.GetViewMatrix();
+            ourShader.setMat4("projection", projection);
+            ourShader.setMat4("view", view);
+            ourShader.setVec3("viewPos", camera.Position);
+            ourShader.setVec3("lightDirection", lightDir);
+
+            // render the loaded model
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, glm::vec3(1.07109f, -5.22503f, 3.59047f)); // translate it to the center
+            model = glm::translate(model, glm::vec3(0.0f, -10.0f, -50.0f)); // far away from camera
+            model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f)); //rotate
+            model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));	// it's a bit too big for our scene, so scale it down
+
+            ourShader.setMat4("model", model);
+            ourModel.Draw(ourShader);
+
+        }
+
+        //{
+        //    draw_cube(ourShader, camera, lightDir, glm::vec3(3.0f, -5.0f, 3.0f), grass_cube, true);
+        //    draw_cube(ourShader, camera, lightDir, glm::vec3(2.0f, -5.0f, 3.0f), brick_cube);
+        //    draw_cube(ourShader, camera, lightDir, glm::vec3(1.0f, -5.0f, 3.0f), stone_cube);
+        //    draw_cube(ourShader, camera, lightDir, glm::vec3(3.0f, -5.0f, 2.0f), stone_brick_cube);
+        //    draw_cube(ourShader, camera, lightDir, glm::vec3(2.0f, -5.0f, 2.0f), wood_cube, true);
+        //    draw_cube(ourShader, camera, lightDir, glm::vec3(1.0f, -5.0f, 2.0f), sand_cube);
+        //}
+
+        //vampire_animation
+        //{   
+        //    aniShader.use();
+        //    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        //    glm::mat4 view = camera.GetViewMatrix();
+        //    aniShader.setMat4("projection", projection);
+        //    aniShader.setMat4("view", view);
+
+        //    auto transforms = animator.GetFinalBoneMatrices();
+        //    for (int i = 0; i < transforms.size(); ++i)
+        //        aniShader.setMat4("finalBonesMatrices[" + std::to_string(i) + "]", transforms[i]);
+
+
+        //    // render the loaded model
+        //    glm::mat4 model = glm::mat4(1.0f);
+        //    model = glm::translate(model, glm::vec3(0.0f, -0.4f, 0.0f)); // translate it down so it's at the center of the scene
+        //    model = glm::scale(model, glm::vec3(.5f, .5f, .5f));	// it's a bit too big for our scene, so scale it down
+        //    aniShader.setMat4("model", model);
+        //    aModel.Draw(aniShader);
+        //}
+
+        //chicken_animation
+        //{
+        //    aniShader.use();
+
+        //    // view/projection transformations
+        //    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        //    glm::mat4 view = camera.GetViewMatrix();
+        //    aniShader.setMat4("projection", projection);
+        //    aniShader.setMat4("view", view);
+
+        //    auto transforms = animator.GetFinalBoneMatrices();
+        //    for (int i = 0; i < transforms.size(); ++i)
+        //        aniShader.setMat4("finalBonesMatrices[" + std::to_string(i) + "]", transforms[i]);
+
+        //    // render the loaded model
+        //    glm::mat4 model = glm::mat4(1.0f);
+        //    model = glm::translate(model, glm::vec3(-0.0397672f, 4.08026f, -22.3267f)); // translate it to the center of the scene
+        //    model = glm::translate(model, glm::vec3(-10.0f, -8.0f, 5.0f));
+        //    model = glm::rotate(model, glm::radians(135.0f), glm::vec3(0.0f, 1.0f, 0.0f)); // rotate some degrees around the Y axis
+        //    model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f)); // rotate 90 degrees around the X axis
+        //    model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f)); // scale it down
+        //    aniShader.setMat4("model", model);
+        //    aModel.Draw(aniShader);
+        //}
+
+        
+        //bee_animation now not available
+        //{
+        //    beeShader.use();
+
+        //    // view/projection transformations
+        //    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        //    glm::mat4 view = camera.GetViewMatrix();
+        //    beeShader.setMat4("projection", projection);
+        //    beeShader.setMat4("view", view);
+
+        //    auto transforms = beeAnimator.GetFinalBoneMatrices();
+        //    for (int i = 0; i < transforms.size(); ++i)
+        //        beeShader.setMat4("finalBonesMatrices[" + std::to_string(i) + "]", transforms[i]);
+
+        //    // render the loaded model
+        //    glm::mat4 model = glm::mat4(1.0f);
+        //    //model = glm::translate(model, glm::vec3(-3.75174f, -3.09131f, 9.10885f)); // translate it to the center of the scene
+        //    //model = glm::translate(model, glm::vec3(-10.0f, -8.0f, 5.0f));
+        //    //model = glm::rotate(model, glm::radians(135.0f), glm::vec3(0.0f, 1.0f, 0.0f)); // rotate some degrees around the Y axis
+        //    //model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f)); // rotate 90 degrees around the X axis
+        //    model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f)); // scale it down
+        //    beeShader.setMat4("model", model);
+        //    beeModel.Draw(beeShader);
+        //}
+
+        // draw skybox as last
+        {
+            glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
+            skyboxShader.use();
+            glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+            glm::mat4 view = glm::mat4(glm::mat3(camera.GetViewMatrix())); // remove translation from the view matrix
+            skyboxShader.setMat4("view", view);
+            skyboxShader.setMat4("projection", projection);
+            // skybox cube
+            glBindVertexArray(skyboxVAO);
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+            glBindVertexArray(0);
+            glDepthFunc(GL_LESS); // set depth function back to default
+
+        }
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime * 5.0f);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime * 5.0f);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime * 5.0f);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime * 5.0f);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+// loads a cubemap texture from 6 individual texture faces
+// order:
+// +X (right)
+// -X (left)
+// +Y (top)
+// -Y (bottom)
+// +Z (front)
+// -Z (back)
+// -------------------------------------------------------
+unsigned int loadCubemap(vector<std::string> faces)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+    int width, height, nrChannels;
+    for (unsigned int i = 0; i < faces.size(); i++)
+    {
+        unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data)
+        {
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return textureID;
+}
