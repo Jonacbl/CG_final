98b4485006a74cb096dc08c0b3b579305b4358b8
Jonacbl <chenblin23@mail2.sysu.edu.cn>
2024-12-10 17:33:07 +0800

    models&ground

--- main.cpp
+++ main.cpp
@@ -20,13 +20,14 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
 unsigned int loadCubemap(vector<std::string> faces);
+void draw_cube(Shader& ourShader, Camera& camera, glm::vec3 lightDir, glm::vec3 worldPos, Model cube, bool ro = false);
 
 // settings
 const unsigned int SCR_WIDTH = 1600;
 const unsigned int SCR_HEIGHT = 900;
 
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 8.0f, 10.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
@@ -37,35 +38,8 @@ const glm::vec3 cameraPos = glm::vec3(0.0f, 1.0f, -10.0f);
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-/// @brief Draw a cube, at the given position, read README for more details
-/// @param ourShader shader
-/// @param camera    camera
-/// @param lightDir  light direction
-/// @param worldPos  world position of to draw the model
-/// @param cube      cube model
-/// @param ro        a control flag, by default false
-void draw_cube(Shader &ourShader, Camera &camera, glm::vec3 lightDir, glm::vec3 worldPos, Model cube, bool ro=false) {
-    ourShader.use();
-
-    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-    glm::mat4 view = camera.GetViewMatrix();
-    ourShader.setMat4("projection", projection);
-    ourShader.setMat4("view", view);
-    ourShader.setVec3("viewPos", camera.Position);
-    ourShader.setVec3("lightDirection", lightDir);
-
-    glm::mat4 modelMatrix = glm::mat4(1.0f);
-
-    modelMatrix = glm::translate(modelMatrix, worldPos);
-    modelMatrix = glm::translate(modelMatrix, cameraPos);
-    if (ro) {
-        modelMatrix = glm::rotate(modelMatrix, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-    }
-    modelMatrix = glm::scale(modelMatrix, glm::vec3(0.5f, 0.5f, 0.5f));
-    ourShader.setMat4("model", modelMatrix);
-
-    cube.Draw(ourShader);
-}
+//parallel light
+glm::vec3 lightDir(0.0f, -1.0f, -1.0f);
 
 
 int main()
@@ -116,7 +90,9 @@ int main()
     // build and compile shaders
     // -------------------------
     Shader ourShader("shader/blinn-phong.vs", "shader/blinn-phong.fs");
+    Shader aniShader("shader/ani_shader.vs", "shader/ani_shader.fs");
     Shader skyboxShader("shader/skybox.vs", "shader/skybox.fs");
+    Shader groundShader("shader/ground.vs", "shader/ground.fs");
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -194,18 +170,11 @@ int main()
     skyboxShader.use();
     skyboxShader.setInt("skybox", 0);
 
-    //parallel light
-    glm::vec3 lightDir(0.0f, -1.0f, -1.0f);
-
     // load models
     // -----------
     Model ourModel("resources/autumn-house/source/House_scene_01.fbx");
     ourModel.CalculateCenter();
     
-    //animation model
-    //Model aModel("resources/vampire/dancing_vampire.dae");
-    //Animation danceAnimation("resources/vampire/dancing_vampire.dae", &aModel);
-    //Animator animator(&danceAnimation);
 
     Model aModel("resources/chicken-rig/source/Chicken.FBX");
     Animation danceAnimation("resources/chicken-rig/source/Chicken.FBX", &aModel);
@@ -220,6 +189,48 @@ int main()
     Model brick_cube("resources/brick_cube/scene.gltf");
     Model stone_brick_cube("resources/stone_brick_cube/scene.gltf");
     Model sand_cube("resources/sand_cube/scene.gltf");
+
+    // Draw ground with grass cube
+    unsigned int size = 500;    
+    glm::mat4* groundModelMatrices;
+    groundModelMatrices = new glm::mat4[size * size];
+    for (auto i = 0; i < size * size; i++) {
+        glm::mat4 model = glm::mat4(1.0f);
+        // Calculate world position of the cube according to i
+        int row = i / size;
+        int col = i % size;
+        model = glm::translate(model, glm::vec3(static_cast<float>(col) - static_cast<float>(size)/2.0f, -2.0f, static_cast<float>(row) - static_cast<float>(size) / 2.0f));
+        model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
+
+        groundModelMatrices[i] = model;
+    }
+    // configure instanced array of ground
+    // -------------------------
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, size * size * sizeof(glm::mat4), &groundModelMatrices[0], GL_STATIC_DRAW);
+    for (unsigned int i = 0; i < grass_cube.meshes.size(); i++)
+    {
+        unsigned int VAO = grass_cube.meshes[i].VAO;
+        glBindVertexArray(VAO);
+        // set attribute pointers for matrix (4 times vec4)
+        glEnableVertexAttribArray(3);
+        glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)0);
+        glEnableVertexAttribArray(4);
+        glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(sizeof(glm::vec4)));
+        glEnableVertexAttribArray(5);
+        glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(2 * sizeof(glm::vec4)));
+        glEnableVertexAttribArray(6);
+        glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(3 * sizeof(glm::vec4)));
+
+        glVertexAttribDivisor(3, 1);
+        glVertexAttribDivisor(4, 1);
+        glVertexAttribDivisor(5, 1);
+        glVertexAttribDivisor(6, 1);
+        glBindVertexArray(0);
+    }
+
     // draw in wireframe
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
@@ -236,13 +247,33 @@ int main()
         // input
         // -----
         processInput(window);
-		  //animator.UpdateAnimation(deltaTime/1.5f);
-          //beeAnimator.UpdateAnimation(deltaTime);
+		  animator.UpdateAnimation(deltaTime/1.5f);
+
  
         // render
         // ------
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+         // render a ground with `grass cube`
+        {
+            groundShader.use();
+            glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+            glm::mat4 view = camera.GetViewMatrix();
+            groundShader.setMat4("projection", projection);
+            groundShader.setMat4("view", view);
+            groundShader.setVec3("viewPos", camera.Position);
+            groundShader.setVec3("lightDirection", lightDir);
+            groundShader.setInt("texture_diffuse1", 0);
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, grass_cube.textures_loaded[0].id);
+            for (unsigned int i = 0; i < grass_cube.meshes.size(); i++)
+            {
+                glBindVertexArray(grass_cube.meshes[i].VAO);
+                glDrawElementsInstanced(GL_TRIANGLES, static_cast<unsigned int>(grass_cube.meshes[i].indices.size()), GL_UNSIGNED_INT, 0, size * size);
+                glBindVertexArray(0);
+            }
+        }
 
         //static model of House
         {
@@ -259,7 +290,7 @@ int main()
             // render the loaded model
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, glm::vec3(1.07109f, -5.22503f, 3.59047f)); // translate it to the center
-            model = glm::translate(model, glm::vec3(0.0f, -10.0f, -50.0f)); // far away from camera
+            model = glm::translate(model, glm::vec3(0.0f, 1.0f, -50.0f)); // far away from camera
             model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f)); //rotate
             model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));	// it's a bit too big for our scene, so scale it down
 
@@ -268,86 +299,31 @@ int main()
 
         }
 
-        //{
-        //    draw_cube(ourShader, camera, lightDir, glm::vec3(3.0f, -5.0f, 3.0f), grass_cube, true);
-        //    draw_cube(ourShader, camera, lightDir, glm::vec3(2.0f, -5.0f, 3.0f), brick_cube);
-        //    draw_cube(ourShader, camera, lightDir, glm::vec3(1.0f, -5.0f, 3.0f), stone_cube);
-        //    draw_cube(ourShader, camera, lightDir, glm::vec3(3.0f, -5.0f, 2.0f), stone_brick_cube);
-        //    draw_cube(ourShader, camera, lightDir, glm::vec3(2.0f, -5.0f, 2.0f), wood_cube, true);
-        //    draw_cube(ourShader, camera, lightDir, glm::vec3(1.0f, -5.0f, 2.0f), sand_cube);
-        //}
-
-        //vampire_animation
-        //{   
-        //    aniShader.use();
-        //    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        //    glm::mat4 view = camera.GetViewMatrix();
-        //    aniShader.setMat4("projection", projection);
-        //    aniShader.setMat4("view", view);
-
-        //    auto transforms = animator.GetFinalBoneMatrices();
-        //    for (int i = 0; i < transforms.size(); ++i)
-        //        aniShader.setMat4("finalBonesMatrices[" + std::to_string(i) + "]", transforms[i]);
-
-
-        //    // render the loaded model
-        //    glm::mat4 model = glm::mat4(1.0f);
-        //    model = glm::translate(model, glm::vec3(0.0f, -0.4f, 0.0f)); // translate it down so it's at the center of the scene
-        //    model = glm::scale(model, glm::vec3(.5f, .5f, .5f));	// it's a bit too big for our scene, so scale it down
-        //    aniShader.setMat4("model", model);
-        //    aModel.Draw(aniShader);
-        //}
-
         //chicken_animation
-        //{
-        //    aniShader.use();
+        {
+            aniShader.use();
 
-        //    // view/projection transformations
-        //    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        //    glm::mat4 view = camera.GetViewMatrix();
-        //    aniShader.setMat4("projection", projection);
-        //    aniShader.setMat4("view", view);
+            // view/projection transformations
+            glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+            glm::mat4 view = camera.GetViewMatrix();
+            aniShader.setMat4("projection", projection);
+            aniShader.setMat4("view", view);
+            aniShader.setVec3("lightDirection", lightDir);
 
-        //    auto transforms = animator.GetFinalBoneMatrices();
-        //    for (int i = 0; i < transforms.size(); ++i)
-        //        aniShader.setMat4("finalBonesMatrices[" + std::to_string(i) + "]", transforms[i]);
+            auto transforms = animator.GetFinalBoneMatrices();
+            for (int i = 0; i < transforms.size(); ++i)
+                aniShader.setMat4("finalBonesMatrices[" + std::to_string(i) + "]", transforms[i]);
 
-        //    // render the loaded model
-        //    glm::mat4 model = glm::mat4(1.0f);
-        //    model = glm::translate(model, glm::vec3(-0.0397672f, 4.08026f, -22.3267f)); // translate it to the center of the scene
-        //    model = glm::translate(model, glm::vec3(-10.0f, -8.0f, 5.0f));
-        //    model = glm::rotate(model, glm::radians(135.0f), glm::vec3(0.0f, 1.0f, 0.0f)); // rotate some degrees around the Y axis
-        //    model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f)); // rotate 90 degrees around the X axis
-        //    model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f)); // scale it down
-        //    aniShader.setMat4("model", model);
-        //    aModel.Draw(aniShader);
-        //}
-
-        
-        //bee_animation now not available
-        //{
-        //    beeShader.use();
-
-        //    // view/projection transformations
-        //    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        //    glm::mat4 view = camera.GetViewMatrix();
-        //    beeShader.setMat4("projection", projection);
-        //    beeShader.setMat4("view", view);
-
-        //    auto transforms = beeAnimator.GetFinalBoneMatrices();
-        //    for (int i = 0; i < transforms.size(); ++i)
-        //        beeShader.setMat4("finalBonesMatrices[" + std::to_string(i) + "]", transforms[i]);
-
-        //    // render the loaded model
-        //    glm::mat4 model = glm::mat4(1.0f);
-        //    //model = glm::translate(model, glm::vec3(-3.75174f, -3.09131f, 9.10885f)); // translate it to the center of the scene
-        //    //model = glm::translate(model, glm::vec3(-10.0f, -8.0f, 5.0f));
-        //    //model = glm::rotate(model, glm::radians(135.0f), glm::vec3(0.0f, 1.0f, 0.0f)); // rotate some degrees around the Y axis
-        //    //model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f)); // rotate 90 degrees around the X axis
-        //    model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f)); // scale it down
-        //    beeShader.setMat4("model", model);
-        //    beeModel.Draw(beeShader);
-        //}
+            // render the loaded model
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, glm::vec3(-0.0397672f, 4.08026f, -22.3267f)); // translate it to the center of the scene
+            model = glm::translate(model, glm::vec3(-20.0f, -4.5f, -15.0f));
+            model = glm::rotate(model, glm::radians(135.0f), glm::vec3(0.0f, 1.0f, 0.0f)); // rotate some degrees around the Y axis
+            model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f)); // rotate 90 degrees around the X axis
+            model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f)); // scale it down
+            aniShader.setMat4("model", model);
+            aModel.Draw(aniShader);
+        }
 
         // draw skybox as last
         {
@@ -388,13 +364,17 @@ void processInput(GLFWwindow* window)
         glfwSetWindowShouldClose(window, true);
 
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        camera.ProcessKeyboard(FORWARD, deltaTime * 5.0f);
+        camera.ProcessKeyboard(FORWARD, deltaTime * 10.0f);
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        camera.ProcessKeyboard(BACKWARD, deltaTime * 5.0f);
+        camera.ProcessKeyboard(BACKWARD, deltaTime * 10.0f);
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        camera.ProcessKeyboard(LEFT, deltaTime * 5.0f);
+        camera.ProcessKeyboard(LEFT, deltaTime * 10.0f);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        camera.ProcessKeyboard(RIGHT, deltaTime * 5.0f);
+        camera.ProcessKeyboard(RIGHT, deltaTime * 10.0f);
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+        camera.ProcessKeyboard(UP, deltaTime * 10.0f);
+    if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS)
+        camera.ProcessKeyboard(DOWN, deltaTime * 10.0f);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -473,4 +453,34 @@ unsigned int loadCubemap(vector<std::string> faces)
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 
     return textureID;
+}
+
+/// @brief Draw a cube, at the given position, read README for more details
+/// @param ourShader shader
+/// @param camera    camera
+/// @param lightDir  light direction
+/// @param worldPos  world position of to draw the model
+/// @param cube      cube model
+/// @param ro        a control flag, by default false
+void draw_cube(Shader& ourShader, Camera& camera, glm::vec3 lightDir, glm::vec3 worldPos, Model cube, bool ro) {
+    ourShader.use();
+
+    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    glm::mat4 view = camera.GetViewMatrix();
+    ourShader.setMat4("projection", projection);
+    ourShader.setMat4("view", view);
+    ourShader.setVec3("viewPos", camera.Position);
+    ourShader.setVec3("lightDirection", lightDir);
+
+    glm::mat4 modelMatrix = glm::mat4(1.0f);
+
+    modelMatrix = glm::translate(modelMatrix, worldPos);
+    modelMatrix = glm::translate(modelMatrix, camera.Position);
+    if (ro) {
+        modelMatrix = glm::rotate(modelMatrix, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    }
+    modelMatrix = glm::scale(modelMatrix, glm::vec3(0.5f, 0.5f, 0.5f));
+    ourShader.setMat4("model", modelMatrix);
+
+    cube.Draw(ourShader);
 }
